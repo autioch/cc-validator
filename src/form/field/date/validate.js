a14887d51cac220a78918b5bc21c65006cf1a064
autioch <autioch+github@gmail.com>
2017-04-28 01:50:48 +0200

    Date validation updated.

--- src/form/field/date/validate.js
+++ src/form/field/date/validate.js
@@ -1,5 +1,17 @@
 const dateRegex = /^(\d\d\d\d)-(\d\d)-(\d\d)$/;
 
+const currentDate = new Date();
+
 module.exports = function validate(value) {
-  return dateRegex.test(value);
+  if (!dateRegex.test(value)) {
+    return false;
+  }
+
+  const enteredDate = new Date(value);
+
+  if (isNaN(enteredDate.getTime())) {
+    return false;
+  }
+
+  return enteredDate > currentDate;
 };
